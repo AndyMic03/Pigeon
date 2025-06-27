29876e1e7dfb847449c0ce92f66682ffec122da2
Andreas Michael <ateasm03@gmail.com>
2025-06-28 02:34:30 +0300

    Query Function Replacement
    - Added an optional parameter to all functions that allows for replacement of the query function
    - Revamped the generated index.ts file
    - Added missing semicolumns

--- packages/pigeon/src/config.ts
+++ packages/pigeon/src/config.ts
@@ -10,8 +10,8 @@ import * as path from "node:path";
 
 export function createConfig(dir: string): PigeonError {
     if (fs.existsSync(path.join(dir, ".pigeon.json")))
-        return new PigeonError(1, "", new Error("A Pigeon configuration file already exists."))
+        return new PigeonError(1, "", new Error("A Pigeon configuration file already exists."));
 
     fs.writeFileSync(path.join(dir, ".pigeon.json"), "{\n\t\"host\": \"localhost\",\n\t\"port\": 5432,\n\t\"database\": \"postgres\",\n\t\"username\": \"postgres\",\n\t\"password\": \"xxx\"\n}");
-    return new PigeonError(0, "Configuration file successfully created!", null)
+    return new PigeonError(0, "Configuration file successfully created!", null);
 }
